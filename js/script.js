88de397d0263cb9370e19bb52e49c80e4bfe2251
Adekunle Adedokun <adekunleaedokun6@gmail.com>
2020-11-16 16:06:06 +0100

    Commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+(function parentFunct() {
+    var display =  document.querySelector('.display');
+    var addOne = document.querySelector('.display-plus');
+    var minusOne = document.querySelector('.display-minus');
+    var displayValue = Number(display.childNodes[0].nodeValue);
+
+    function plusOne() {
+        display.textContent = displayValue++ ;
+    }
+
+    function subtractOne() {
+        display.textContent = displayValue-- ;
+    }
+
+    addOne.addEventListener('click', plusOne);
+    minusOne.addEventListener('click', subtractOne);
+
+}())
