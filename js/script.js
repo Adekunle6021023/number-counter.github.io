581fafcea4d5f9ea408aee661515c2dd1238d159
Adekunle Adedokun <adekunleaedokun6@gmail.com>
2020-11-18 16:45:10 +0100

    New Commit

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@
     var addOne = document.querySelector('.display-plus');
     var minusOne = document.querySelector('.display-minus');
     var displayValue = Number(display.childNodes[0].nodeValue);
+    var contain = document.querySelector('.container');
 
     function plusOne() {
         display.textContent = displayValue++ ;
@@ -14,5 +15,6 @@
 
     addOne.addEventListener('click', plusOne);
     minusOne.addEventListener('click', subtractOne);
+
 
 }())
